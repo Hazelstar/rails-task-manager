740c3b24775f988df2a69439d9b72aa7579ce70f
Lola Guerin <lola.guerin@gmail.com>
2020-11-03 12:52:33 +0000

    created my Task model and its corresponding db, set up the routes, the controller and the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # user can list the tasks
+  get '/tasks', to: 'tasks#index'
+
+  # user can add a new task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+
+  # user can view the details of the task
+  get '/tasks/:id', to: 'tasks#show'
+
+  # user can edit a task
+  get '/tasks/:id', to: 'tasks#edit'
+  patch '/tasks/:id/edit', to: 'tasks#update'
+
+  # user can remove a task
+  delete '/tasks', to: 'tasks#destroy'
 end
